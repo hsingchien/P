94a6c6acf3396ae42cd3371148310b3530c76655
XIngjian Zhang <hsingchien.zhang@gmail.com>
2018-01-20 12:28:47 -0600

    Add files via upload

---
+++ week 3/ChooseTopSimilarityFactors.m
@@ -0,0 +1,35 @@
+function factors = ChooseTopSimilarityFactors (allFactors, F)
+% This function chooses the similarity factors with the highest similarity
+% out of all the possibilities.
+%
+% Input:
+%   allFactors: An array of all the similarity factors.
+%   F: The number of factors to select.
+%
+% Output:
+%   factors: The F factors out of allFactors for which the similarity score
+%     is highest.
+%
+% Hint: Recall that the similarity score for two images will be in every
+%   factor table entry (for those two images' factor) where they are
+%   assigned the same character value.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+% If there are fewer than F factors total, just return all of them.
+if (length(allFactors) <= F)
+    factors = allFactors;
+    return;
+end
+
+% Your code here:
+vals = [];
+for i = 1:length(allFactors)
+   vals = [vals, allFactors(i).val(1)]; 
+end
+[temp, orig] = sort(vals, 'descend');
+
+factors = allFactors(sort(orig(1:F))); %%% REMOVE THIS LINE
+
+end
+
